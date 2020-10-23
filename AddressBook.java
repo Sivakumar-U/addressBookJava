73d5bc3b313c68c722b1c65f6d7fc3051ce9a72d
Sivakumar-U <usivakumarreddy@gmail.com>
2020-10-23 19:49:42 +0530

    Added: Ability to add a new contact to address book

--- AddressBook.java
+++ AddressBook.java
@@ -1,8 +1,41 @@
 package com.blz.addressbook;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
-	
+
+	public static List<ContactPerson> person = new ArrayList<ContactPerson>();
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
 		System.out.println("Hello there...Welcome to address book problem");
+		AddressBook ab = new AddressBook();
+		ab.addContact();
+	}
+
+	private void addContact() {
+		System.out.println("Enter contact details");
+		System.out.println("Enter FirstName:");
+		String firstName = sc.next();
+		System.out.println("Enter LastName:");
+		String lastName = sc.next();
+		System.out.println("Enter Address:");
+		String address = sc.next();
+		System.out.println("Enter City:");
+		String city = sc.next();
+		System.out.println("Enter State:");
+		String state = sc.next();
+		System.out.println("Enter Zip:");
+		int zip = sc.nextInt();
+		System.out.println("Enter PhoneNumber:");
+		int phoneNumber = sc.nextInt();
+		System.out.println("Enter Email:");
+		String email = sc.next();
+		ContactPerson contact = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
+		person.add(contact);
+		System.out.println("Details are uploaded successfully..");
+
 	}
 }
--- ContactPerson.java
+++ ContactPerson.java
@@ -7,12 +7,12 @@ public class ContactPerson {
 	public String address;
 	public String city;
 	public String state;
-	public long zip;
-	public long phoneNumber;
+	public int zip;
+	public int phoneNumber;
 	public String email;
 
-	public ContactPerson(String firstName, String lastName, String address, String city, String state, long zip,
-			long phoneNumber, String email) {
+	public ContactPerson(String firstName, String lastName, String address, String city, String state, int zip,
+			int phoneNumber, String email) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.address = address;
@@ -20,6 +20,70 @@ public class ContactPerson {
 		this.state = state;
 		this.zip = zip;
 		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZip() {
+		return zip;
+	}
+
+	public void setZip(int zip) {
+		this.zip = zip;
+	}
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
 		this.email = email;
 	}
 }
